1e341813681a9b18d560ab284c990261b0032273
Andrey Kotsar <andrey.kotsar@gmail.com>
2023-09-22 17:49:26 +1200

    added strategies & implemented one for Sulfuras

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -10,6 +10,41 @@ export class Item {
   }
 }
 
+interface ItemUpdateStrategy {
+  /**
+   * Determines whether the specified item qualifies for this strategy.
+   * @param item The item to check.
+   * @returns `true` if the item qualifies, `false` otherwise.
+   */
+  isForItem(item: Item): boolean;
+
+  /**
+   * Updates the item quality and sellIn values according to the strategy rules.
+   * @param item The item to update.
+   */
+  updateItem(item: Item): void;
+}
+
+class LegendaryItemUpdateStrategy implements ItemUpdateStrategy {
+  isForItem(item: Item): boolean {
+    return item.name.startsWith("Sulfuras");
+  }
+
+  updateItem(item: Item): void {
+    // Nothing changes for legendary items.
+  }
+}
+
+/**
+ * The list of supported item update strategies.
+ *
+ * NOTE: The order of items in this array is important. Several
+ * update strategies can return `true` from `isForItem()` for
+ * the same item. The first update strategy to return `true` will
+ * be used for the item.
+ */
+const ITEM_UPDATE_STRATEGIES = [new LegendaryItemUpdateStrategy()];
+
 export class GildedRose {
   items: Array<Item>;
 
@@ -20,14 +55,19 @@ export class GildedRose {
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
       const item = this.items[i];
+
+      const updateStrategy = this._getUpdateStrategyForItem(item);
+      if (updateStrategy) {
+        updateStrategy.updateItem(item);
+        continue;
+      }
+
       if (
         item.name != "Aged Brie" &&
         item.name != "Backstage passes to a TAFKAL80ETC concert"
       ) {
         if (item.quality > 0) {
-          if (item.name != "Sulfuras, Hand of Ragnaros") {
-            item.quality -= 1;
-          }
+          item.quality -= 1;
         }
       } else {
         if (item.quality < 50) {
@@ -46,16 +86,12 @@ export class GildedRose {
           }
         }
       }
-      if (item.name != "Sulfuras, Hand of Ragnaros") {
-        item.sellIn -= 1;
-      }
+      item.sellIn -= 1;
       if (item.sellIn < 0) {
         if (item.name != "Aged Brie") {
           if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
             if (item.quality > 0) {
-              if (item.name != "Sulfuras, Hand of Ragnaros") {
-                item.quality -= 1;
-              }
+              item.quality -= 1;
             }
           } else {
             item.quality = 0;
@@ -69,5 +105,13 @@ export class GildedRose {
     }
 
     return this.items;
+  }
+
+  _getUpdateStrategyForItem(item: Item): ItemUpdateStrategy | undefined {
+    for (const strategy of ITEM_UPDATE_STRATEGIES) {
+      if (strategy.isForItem(item)) {
+        return strategy;
+      }
+    }
   }
 }
